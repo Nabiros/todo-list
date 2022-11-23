007484861c82d482b7d4448b3d690c78756ad969
Nazar Khizhnyak <77328105+Nabiros@users.noreply.github.com>
2022-11-23 15:58:00 +0300

    added base structure

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
+// const swagger = require('swagger');
+// const swaggerDocument = require('./swagger.json');
+
+const app = express();
+
+module.exports = app;
---
+++ bin/server.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const app = require('../app');
+
+// const { DB_HOST, PORT } = process.env;
+
+mongoose
+    .connect(DB_HOST)
+    .then(() => {
+        console.log(`Connection is success on port: ${PORT}`);
+        return app.listen(PORT);
+    })
+    .catch((err) => {
+        console.log(err.message);
+        process.exit(1);
+    });
